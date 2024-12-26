6840cf0f85753478ae95924a1e30119bc35b94ae
Douglas Teixeira <douglasdocouto@gmail.com>
2024-12-26 22:48:04 +0000

    Rename variables

--- concurrent-counters/ExactCounter.h
+++ concurrent-counters/ExactCounter.h
@@ -3,20 +3,20 @@
 
 class ExactCounter {
  private:
-  int64_t global_counter_{0};
-  std::mutex global_counter_mutex_;
+  int64_t counter_{0};
+  std::mutex mutex_;
 
  public:
   ExactCounter() {}
 
   int64_t update(int64_t amount) {
-    std::unique_lock<std::mutex> lock{global_counter_mutex_};
-    global_counter_ += amount;
-    return global_counter_;
+    std::unique_lock<std::mutex> lock{mutex_};
+    counter_ += amount;
+    return counter_;
   }
 
   int64_t get() {
-    std::unique_lock<std::mutex> lock{global_counter_mutex_};
-    return global_counter_;
+    std::unique_lock<std::mutex> lock{mutex_};
+    return counter_;
   }
 };
