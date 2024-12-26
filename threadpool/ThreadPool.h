1bce393d69e666e698f4d50521625a399c127f2e
Douglas Teixeira <douglasdocouto@gmail.com>
2024-12-26 12:24:04 +0000

    Polish benchmarks

--- threadpool/ThreadPool.h
+++ threadpool/ThreadPool.h
@@ -4,13 +4,95 @@
 #include <condition_variable>
 #include <deque>
 #include <functional>
-#include <iostream>
 #include <mutex>
 #include <thread>
 #include <utility>
 #include <vector>
 
-class SimpleTaskQueue {
+
+class BasicThreadSafeQueue {
+  std::deque<std::function<void()>> _queue;
+  std::mutex _mutex;
+  bool _done{false};
+
+ public:
+  bool pop(std::function<void()>& x) {
+    std::unique_lock<std::mutex> lock{_mutex, std::try_to_lock};
+    if (!lock || _queue.empty()) {
+      return false;
+    }
+    x = std::move(_queue.front());
+    _queue.pop_front();
+    return true;
+  }
+
+  template <typename F>
+  bool push(F&& f) {
+    {
+      std::unique_lock<std::mutex> lock{_mutex, std::try_to_lock};
+      if (!lock) {
+        return false;
+      }
+      _queue.emplace_back(std::forward<F>(f));
+    }
+    return true;
+  }
+
+  bool done() {
+    std::unique_lock<std::mutex> lock{_mutex};
+    _done = true;
+    return true;
+  }
+
+  bool is_done() {
+    std::unique_lock<std::mutex> lock{_mutex};
+    return _done;
+  }
+};
+
+class BasicThreadPool {
+  const unsigned _nthreads{std::thread::hardware_concurrency()};
+  std::vector<std::thread> _threads;
+  BasicThreadSafeQueue _queue;
+
+  void run() {
+    while (true) {
+      std::function<void()> task;
+      if (_queue.is_done()) {
+        break;
+      }
+      if (!_queue.pop(task)) {
+        std::this_thread::yield();
+        continue;
+      }
+      task();
+    }
+  }
+
+ public:
+  BasicThreadPool() {
+    _threads.reserve(_nthreads);
+    for (unsigned i = 0; i != _nthreads; ++i) {
+      _threads.emplace_back([&]() { run(); });
+    }
+  }
+
+  ~BasicThreadPool() {
+    _queue.done();  // XXX: Without this we hang in the dtor
+    for (auto& t : _threads) {
+      t.join();
+    }
+  }
+
+  template <typename F>
+  void submit(F&& f) {
+    while (!_queue.push(std::forward<F>(f))) {
+      std::this_thread::yield();
+    }
+  }
+};
+
+class SimpleThreadSafeQueue {
   std::deque<std::function<void()>> _queue;
   std::mutex _mutex;
   std::condition_variable _ready;
@@ -52,7 +134,7 @@ class SimpleTaskQueue {
 class SimpleThreadPool {
   const unsigned _nthreads{std::thread::hardware_concurrency()};
   std::vector<std::thread> _threads;
-  SimpleTaskQueue _queue;
+  SimpleThreadSafeQueue _queue;
 
   void run() {
     while (true) {
@@ -88,7 +170,7 @@ class SimpleThreadPool {
   }
 };
 
-class TaskQueue {
+class ThreadSafeQueue {
   std::deque<std::function<void()>> _queue;
   std::mutex _mutex;
   std::condition_variable _ready;
@@ -112,7 +194,7 @@ class TaskQueue {
       if (!lock) {
         return false;
       }
-      _queue.emplace_back(forward<F>(f));
+      _queue.emplace_back(std::forward<F>(f));
     }
     _ready.notify_one();
     return true;
@@ -134,7 +216,7 @@ class TaskQueue {
     if (_queue.empty()) {
       return false;
     }
-    x = move(_queue.front());
+    x = std::move(_queue.front());
     _queue.pop_front();
     return true;
   }
@@ -143,7 +225,7 @@ class TaskQueue {
   void push(F&& f) {
     {
       std::unique_lock<std::mutex> lock{_mutex};
-      _queue.emplace_back(forward<F>(f));
+      _queue.emplace_back(std::forward<F>(f));
     }
     _ready.notify_one();
   }
@@ -152,7 +234,7 @@ class TaskQueue {
 class ThreadPool {
   const unsigned _nthreads{std::thread::hardware_concurrency()};
   std::vector<std::thread> _threads;
-  std::vector<TaskQueue> _queues{_nthreads};
+  std::vector<ThreadSafeQueue> _queues{_nthreads};
   std::atomic<unsigned> _index{0};
   const unsigned KMaxIterations = 32;
 
@@ -192,10 +274,10 @@ class ThreadPool {
   void submit(F&& f) {
     auto i = _index++;
     for (unsigned n = 0; n != _nthreads; ++n) {
-      if (_queues[(i + n) % _nthreads].try_push(forward<F>(f))) {
+      if (_queues[(i + n) % _nthreads].try_push(std::forward<F>(f))) {
         return;
       }
     }
-    _queues[i % _nthreads].push(forward<F>(f));
+    _queues[i % _nthreads].push(std::forward<F>(f));
   }
 };
--- threadpool/ThreadPoolTests.cpp
+++ threadpool/ThreadPoolTests.cpp
@@ -7,6 +7,34 @@
 #include "gtest/gtest.h"
 #include "ThreadPool.h"
 
+TEST(BasicThreadPool, SingleTask) {
+    BasicThreadPool pool;
+    std::atomic<bool> taskExecuted{false};
+
+    pool.submit([&taskExecuted]() {
+        taskExecuted = true;
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    EXPECT_TRUE(taskExecuted);
+}
+
+TEST(BasicThreadPool, MultipleTasks) {
+    BasicThreadPool pool;
+    std::atomic<int> counter{0};
+
+    const int numTasks = 100;
+    for (int i = 0; i < numTasks; ++i) {
+        pool.submit([&counter]() {
+            counter++;
+        });
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    EXPECT_EQ(counter, numTasks);
+}
+
+
 TEST(SimpleThreadPool, SingleTask) {
     SimpleThreadPool pool;
     std::atomic<bool> taskExecuted{false};
